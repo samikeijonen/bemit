f5cf026666c63bfe6291803eed7daeb0ad04b44e
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-06-01 08:49:46 +0300

    Add post and body classes.

--- inc/functions-filters.php
+++ inc/functions-filters.php
@@ -28,6 +28,10 @@ function bemit_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
 
 	$atts['class'] = 'menu__anchor menu__anchor--' . $theme_location;
 
+	if ( in_array( 'current-page-ancestor', $item->classes, true ) || in_array( 'current-menu-ancestor', $item->classes, true ) ) {
+		$atts['class'] .= ' is-ancestor';
+	}
+
 	if ( in_array( 'current-menu-item', $item->classes, true ) ) {
 		$atts['class'] .= ' is-active';
 	}
@@ -58,7 +62,7 @@ add_filter( 'nav_menu_link_attributes', 'bemit_nav_menu_link_attributes', 10, 4 
  * @param int     $current_page ID of the current page.
  */
 function bemit_page_menu_link_attributes( $atts, $page, $depth, $args, $current_page ) {
-	$atts['class'] = 'menu__anchor menu__anchor--sub-menu';
+	$atts['class'] = 'menu__anchor menu__anchor--sub-pages';
 
 	if ( $current_page === $page->ID ) {
 		$atts['class'] .= ' is-active';
@@ -117,18 +121,18 @@ add_filter( 'nav_menu_css_class', 'bemit_nav_menu_css_class', 10, 4 );
  * @param int      $current_page ID of the current page.
  */
 function bemit_page_css_class( $css_class, $page, $depth, $args, $current_page ) {
-	$css_class['class'] = 'menu__item menu__item--sub-menu';
+	$css_class = [ 'menu__item menu__item--sub-pages' ];
 
 	if ( in_array( 'page_item_has_children', $css_class, true ) ) {
-		$css_class['class'] .= ' has-children';
+		$css_class[] = 'has-children';
 	}
 
 	if ( in_array( 'current_page_ancestor', $css_class, true ) ) {
-		$css_class['class'] .= ' menu__item--ancestor';
+		$css_class[] = 'menu__item--ancestor';
 	}
 
 	if ( 0 === $depth ) {
-		$css_class['class'] .= ' is-top-level';
+		$css_class[] = 'is-top-level';
 	}
 
 	return $css_class;
@@ -153,14 +157,51 @@ add_filter( 'nav_menu_submenu_css_class', 'bemit_nav_menu_submenu_css_class', 10
  * Remove hentry and add entry at the same time.
  *
  * @param string[] $classes An array of post class names.
+ * @param string[] $class   An array of additional class names added to the post.
+ * @param int      $post_id The post ID.
  */
-function bemit_entry_classes( $classes ) {
-	// Remove .hentry class.
-	$classes = array_diff( $classes, [ 'hentry' ] );
+function bemit_entry_classes( $classes, $class, $post_id ) {
+	if ( is_admin() ) {
+		return $classes;
+	}
 
-	// Add .entry class.
+	$classes = [];
+	$post    = get_post( $post_id );
+
+	// Entry class.
 	$classes[] = 'entry';
+
+	// Post field classes.
+	$classes[] = sprintf( 'entry--%s', $post_id );
+	$classes[] = sprintf( 'entry--type-%s', get_post_type() );
+
+	// Author class.
+	$classes[] = sprintf(
+		'entry--author-%s',
+		sanitize_html_class( get_the_author_meta( 'user_nicename' ), get_the_author_meta( 'ID' ) )
+	);
 
 	return $classes;
 }
-add_filter( 'post_class', 'bemit_entry_classes' );
+add_filter( 'post_class', 'bemit_entry_classes', 10, 3 );
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function bemit_body_classes( $classes ) {
+	// Adds a class of hfeed to non-singular pages.
+	if ( ! is_singular() ) {
+		$classes[] = 'hfeed';
+	}
+
+	// Adds a class of no-sidebar when there is no sidebar present.
+	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+		$classes[] = 'no-sidebar';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'bemit_body_classes' );
