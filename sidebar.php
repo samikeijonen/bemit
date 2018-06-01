6a422ff06e69089a82cd82d4eb0edbf7500aec6b
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-06-01 08:44:47 +0300

    Add sub menu to sidebar.

--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,12 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+<aside class="site-widgets">
+	<?php
+		// Show subpages.
+		bemit_sub_pages_navigation();
+
+		// Show sidebar widgets.
+		dynamic_sidebar( 'sidebar-1' );
+	?>
 </aside><!-- #secondary -->
