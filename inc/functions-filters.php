67fbb505a036d69121b50afb98ce71d3563034b0
Sami Keijonen <sami.keijonen@foxnet.fi>
2018-06-01 11:38:13 +0300

    Change order of filters.

--- inc/functions-filters.php
+++ inc/functions-filters.php
@@ -8,6 +8,35 @@
  */
 
 /**
+ * Filters the CSS classes applied to a menu item's list item element.
+ *
+ * @param string[] $classes Array of the CSS classes that are applied to the menu item's `<li>` element.
+ * @param WP_Post  $item    The current menu item.
+ * @param stdClass $args    An object of wp_nav_menu() arguments.
+ * @param int      $depth   Depth of menu item. Used for padding.
+ */
+function bemit_nav_menu_css_class( $classes, $item, $args, $depth ) {
+	$theme_location = $args->theme_location ? $args->theme_location : 'default';
+
+	$_classes = [ 'menu__item' ];
+
+	// Add theme location class.
+	$_classes[] = 'menu__item--' . $theme_location;
+
+	if ( 0 === $depth ) {
+		$_classes[] = 'is-top-level';
+	}
+
+	// Add a class if the menu item has children.
+	if ( in_array( 'menu-item-has-children', $classes, true ) ) {
+		$_classes[] = 'has-children';
+	}
+
+	return $_classes;
+}
+add_filter( 'nav_menu_css_class', 'bemit_nav_menu_css_class', 10, 4 );
+
+/**
  * Filters the WP nav menu link attributes.
  *
  * @param array    $atts {
@@ -49,6 +78,36 @@ function bemit_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
 add_filter( 'nav_menu_link_attributes', 'bemit_nav_menu_link_attributes', 10, 4 );
 
 /**
+ * Filters the list of CSS classes to include with each page item in the list.
+ *
+ * @see wp_list_pages()
+ *
+ * @param string[] $css_class    An array of CSS classes to be applied to each list item.
+ * @param WP_Post  $page         Page data object.
+ * @param int      $depth        Depth of page, used for padding.
+ * @param array    $args         An array of arguments.
+ * @param int      $current_page ID of the current page.
+ */
+function bemit_page_css_class( $css_class, $page, $depth, $args, $current_page ) {
+	$css_class = [ 'menu__item menu__item--sub-pages' ];
+
+	if ( in_array( 'page_item_has_children', $css_class, true ) ) {
+		$css_class[] = 'has-children';
+	}
+
+	if ( in_array( 'current_page_ancestor', $css_class, true ) ) {
+		$css_class[] = 'menu__item--ancestor';
+	}
+
+	if ( 0 === $depth ) {
+		$css_class[] = 'is-top-level';
+	}
+
+	return $css_class;
+}
+add_filter( 'page_css_class', 'bemit_page_css_class', 10, 5 );
+
+/**
  * Filters the HTML attributes applied to a page menu item's anchor element.
  *
  * @param array   $atts {
@@ -79,65 +138,6 @@ function bemit_page_menu_link_attributes( $atts, $page, $depth, $args, $current_
 	return $atts;
 }
 add_filter( 'page_menu_link_attributes', 'bemit_page_menu_link_attributes', 10, 5 );
-
-/**
- * Filters the CSS classes applied to a menu item's list item element.
- *
- * @param string[] $classes Array of the CSS classes that are applied to the menu item's `<li>` element.
- * @param WP_Post  $item    The current menu item.
- * @param stdClass $args    An object of wp_nav_menu() arguments.
- * @param int      $depth   Depth of menu item. Used for padding.
- */
-function bemit_nav_menu_css_class( $classes, $item, $args, $depth ) {
-	$theme_location = $args->theme_location ? $args->theme_location : 'default';
-
-	$_classes = [ 'menu__item' ];
-
-	// Add theme location class.
-	$_classes[] = 'menu__item--' . $theme_location;
-
-	if ( 0 === $depth ) {
-		$_classes[] = 'is-top-level';
-	}
-
-	// Add a class if the menu item has children.
-	if ( in_array( 'menu-item-has-children', $classes, true ) ) {
-		$_classes[] = 'has-children';
-	}
-
-	return $_classes;
-}
-add_filter( 'nav_menu_css_class', 'bemit_nav_menu_css_class', 10, 4 );
-
-/**
- * Filters the list of CSS classes to include with each page item in the list.
- *
- * @see wp_list_pages()
- *
- * @param string[] $css_class    An array of CSS classes to be applied to each list item.
- * @param WP_Post  $page         Page data object.
- * @param int      $depth        Depth of page, used for padding.
- * @param array    $args         An array of arguments.
- * @param int      $current_page ID of the current page.
- */
-function bemit_page_css_class( $css_class, $page, $depth, $args, $current_page ) {
-	$css_class = [ 'menu__item menu__item--sub-pages' ];
-
-	if ( in_array( 'page_item_has_children', $css_class, true ) ) {
-		$css_class[] = 'has-children';
-	}
-
-	if ( in_array( 'current_page_ancestor', $css_class, true ) ) {
-		$css_class[] = 'menu__item--ancestor';
-	}
-
-	if ( 0 === $depth ) {
-		$css_class[] = 'is-top-level';
-	}
-
-	return $css_class;
-}
-add_filter( 'page_css_class', 'bemit_page_css_class', 10, 5 );
 
 /**
  * Adds a custom class to the submenus in nav menus.
